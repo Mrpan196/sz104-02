450f4292de409c361c466b318368953ed6612fe0
潘锦福 <1401644385@qq.com>
2020-11-20 15:19:55 +0800

    创建分支

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -7,7 +7,7 @@ package com.itheima.pojo;
  * @create: 2020-11-20 14:01
  **/
 public class User {
-    private String username;
+    private String username;//用户名
     private String password;
     private int age;//年龄a
     private int agea;//aadsad
