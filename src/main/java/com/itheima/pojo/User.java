58ff670020bef263bd7f3db26d4eb9fdfeb446db
潘锦福 <1401644385@qq.com>
2020-11-20 14:29:28 +0800

    初始化

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: Mr.Pan
+ * @create: 2020-11-20 14:01
+ **/
+public class User {
+    private String username;
+    private String password;
+}
